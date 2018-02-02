fea14605526a101db7b92699f4894eb9d1607f35
Unknown <rmh472@nyu.edu>
2018-02-02 14:57:44 -0500

    Lots of stuff the up dash doesnt work?

--- Incubus/Assets/Movement_Controller.cs
+++ Incubus/Assets/Movement_Controller.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement_Controller : MonoBehaviour {
+public class Movement_Controller : MonoBehaviour
+{
+    public enum PlayerState { walk, dash, attack};
+    public PlayerState state = PlayerState.walk;
 
     public KeyCode rightKey;
     public KeyCode leftKey;
     public KeyCode upKey;
     public KeyCode downKey;
+    public KeyCode dashKey;
 
     public float speed;
+    float dashcount = 20;
 
-    Vector2 moveDirection = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
 
     Rigidbody2D rb;
 
@@ -23,30 +28,60 @@ public class Movement_Controller : MonoBehaviour {
 
     void Update()
     {
-        moveDirection *= 0.8f;
-
-        if (Input.GetKey(rightKey))
+        if(state == PlayerState.dash)
         {
-            moveDirection += Vector2.right;
+            dashcount--;
+            moveDirection *= 0.8f;
+            if (moveDirection.z == 1)
+            {
+                moveDirection += Vector3.right;
+            }
+            if (moveDirection.z == 3)
+            {
+                moveDirection += Vector3.left;
+            }
+            if (moveDirection.z == 0)
+            {
+                moveDirection += Vector3.up;
+            }
+            if (moveDirection.z == 2)
+            {
+                moveDirection += Vector3.down;
+            }
+            if (dashcount <= 0)
+                state = PlayerState.walk;
         }
-        if (Input.GetKey(leftKey))
+        if (state == PlayerState.walk)
         {
-            moveDirection += Vector2.left;
-        }
-        if (Input.GetKey(upKey))
-        {
-            moveDirection += Vector2.up;
-        }
-        if (Input.GetKey(downKey))
-        {
-            moveDirection += Vector2.down;
+            moveDirection *= 0.75f;
+            if (Input.GetKey(rightKey))
+            {
+                moveDirection += Vector3.right;
+                moveDirection.z = 1;
+            }
+            if (Input.GetKey(leftKey))
+            {
+                moveDirection += Vector3.left;
+                moveDirection.z = 3;
+            }
+            if (Input.GetKey(upKey))
+            {
+                moveDirection += Vector3.up;
+                moveDirection.z = 0;
+            }
+            if (Input.GetKey(downKey))
+            {
+                moveDirection += Vector3.down;
+                moveDirection.z = 2;
+            }
+            DashCheck();
         }
         Wrap();
     }
 
     void FixedUpdate()
     {
-        Vector2 pos = (Vector2)transform.position + (moveDirection * speed * Time.deltaTime);
+        Vector2 pos = transform.position + (moveDirection * DashMod(speed) * Time.deltaTime);
         rb.MovePosition(pos);
     }
 
@@ -77,4 +112,20 @@ public class Movement_Controller : MonoBehaviour {
             transform.position = pos;
         }
     }
-}
+
+    void DashCheck()
+    {
+        if (Input.GetKeyDown(dashKey))
+        {
+            dashcount = 20;
+            state = PlayerState.dash;
+        }
+    }
+    float DashMod(float speed)
+    {
+        float s = speed;
+        if (state == PlayerState.dash)
+            s *=6;
+        return s;
+    }
+}
