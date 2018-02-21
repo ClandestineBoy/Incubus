05b026cab85f1b0e2656fda99ca66b7f7d6035cc
Unknown <rmh472@nyu.edu>
2018-02-21 01:53:53 -0500

    Thar be an enemy, kinda

--- Incubus/Assets/Scripts/Bullet_Controller.cs
+++ Incubus/Assets/Scripts/Bullet_Controller.cs
@@ -9,17 +9,19 @@ public class Bullet_Controller : MonoBehaviour {
     Vector2 anchor;
 
     public float speed;
+    public float damage;
     Vector3 moveDirection;
 
     Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
+        damage = 1;
         rb = GetComponent<Rigidbody2D>();
         transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
         anchor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         moveDirection = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y);
-
+        
     }
 	
 	// Update is called once per frame
@@ -36,7 +38,15 @@ public class Bullet_Controller : MonoBehaviour {
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag != GameObject.FindGameObjectWithTag("Player").tag)
-        Destroy(gameObject);  
+        
+        if (collision.gameObject.tag != GameObject.FindGameObjectWithTag("Player").tag && collision.gameObject.tag != GameObject.FindGameObjectWithTag("Bullet").tag)
+        { 
+            Destroy(gameObject);
+        }
+       /* if (collision.gameObject.tag == GameObject.FindGameObjectWithTag("Enemy").tag)
+        {
+            var other = collision.GetComponent("Enemy_Controller");
+            other.TakeDamage(damage);
+        }*/
     }
 }
--- Incubus/Assets/Scripts/Enemy_Controller.cs
+++ Incubus/Assets/Scripts/Enemy_Controller.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Enemy_Controller : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    Rigidbody2D rb;
+    Vector3 moveDirection;
+    public float speed;
+    public bool chase;
+    public bool shoot;
+
+    public float maxhp;
+    public float hp;
+
 	void Start () {
-       // rb = GetComponent<Rigidbody2D>();
+       hp = maxhp;
+       rb = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-}
+        moveDirection.x *= 0.8f;
+        moveDirection.y *= 0.8f;
+
+        if(chase == true)
+        moveDirection = new Vector2(GameObject.FindGameObjectWithTag("Player").transform.position.x - transform.position.x, GameObject.FindGameObjectWithTag("Player").transform.position.y - transform.position.y); 
+
+        moveDirection.Normalize();
+
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 pos;
+        pos = transform.position + (moveDirection * speed * Time.deltaTime);
+        rb.MovePosition(pos);
+    }
+    void TakeDamge(float damage)
+    {
+        hp -= damage;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == GameObject.FindGameObjectWithTag("Bullet").tag)
+        {
+            hp -= 1;
+        }
+    }
+    }
